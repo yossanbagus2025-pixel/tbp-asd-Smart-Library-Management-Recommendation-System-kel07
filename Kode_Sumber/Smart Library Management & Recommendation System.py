47e08c17829df51cb0ffb0fd0118061c019646ee
DaffaRifqiMuafa <daffa955.2025@student.uny.ac.id>
2026-05-16 15:06:04 +0700

    melengkapi class GraphRekBuku dan fungsi nya sebagai dasar operasi yang berkaitan dengan  Graph

--- Kode_Sumber/Smart Library Management & Recommendation System.py
+++ Kode_Sumber/Smart Library Management & Recommendation System.py
@@ -213,17 +213,50 @@ class BSTKatalog:
 
 
 class GraphRekBuku:
-    """Graf tak-berarah berbobot: edge (A,B,w) = ko-pinjam frekuensi w."""
     def __init__(self):
-        self.adj = {} # isbn -> [(isbn, freq)]
+        self.adj = {}
+
+    def add_vertex(self, isbn):
+        if isbn not in self.adj:
+            self.adj[isbn] = []
 
     def add_copinjam(self, isbn_a, isbn_b):
-        """Tambah atau naikkan bobot edge. Big-O: O(deg)."""
-        pass # TODO: implementasikan
+        self.add_vertex(isbn_a)
+        self.add_vertex(isbn_b)
+        found = False
+        for i, (isbn, freq) in enumerate(self.adj[isbn_a]):
+            if isbn == isbn_b:
+                self.adj[isbn_a][i] = (isbn, freq + 1)
+                found = True
+                break
+        if not found:
+            self.adj[isbn_a].append((isbn_b, 1))
+        found = False
+        for i, (isbn, freq) in enumerate(self.adj[isbn_b]):
+            if isbn == isbn_a:
+                self.adj[isbn_b][i] = (isbn, freq + 1)
+                found = True
+                break
+        if not found:
+            self.adj[isbn_b].append((isbn_a, 1))
 
     def rekomendasikan(self, isbn, max_hop=2):
-        """BFS hingga max_hop. Big-O: O(V+E)."""
-        pass # TODO: gunakan Queue Linked List sendiri
+        if isbn not in self.adj:
+            return []
+        visited = set([isbn])
+        result = []
+        queue = Queue()
+        queue.enqueue((isbn, 0))
+        while not queue.is_empty():
+            current, hop = queue.dequeue()
+            if hop >= max_hop:
+                continue
+            for tetangga, freq in self.adj[current]:
+                if tetangga not in visited:
+                    visited.add(tetangga)
+                    result.append((tetangga, freq))
+                    queue.enqueue((tetangga, hop + 1))
+        return result
 
 def generate_koleksi(n=80):
     kata = ['Algoritma','Jaringan','Python','Data','Digital',
