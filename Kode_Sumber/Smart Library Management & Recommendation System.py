9c8ad9b5358f6012119d560f38ceeea2c101bea5
alvinandra20G-stack <muhammaderji.2025@student.uny.ac.id>
2026-05-16 14:29:25 +0700

    melengkapi class BST Node dan BSTKatalog

--- Kode_Sumber/Smart Library Management & Recommendation System.py
+++ Kode_Sumber/Smart Library Management & Recommendation System.py
@@ -131,19 +131,86 @@ class BSTKatalog:
 
     def insert(self, buku):
         """Big-O: O(log n) rata-rata. Kunci = buku.isbn."""
-        pass # TODO: implementasikan
+        if self.root is None:
+            self.root = BSTNode(buku)
+        else:
+            return self.insert_recursive(self.root, buku)
+
+    def insert_recursive(self, node, buku):
+        if buku.isbn < node.buku.isbn:
+            if node.left is None:
+                node.left = BSTNode(buku)
+            else:
+                return self.insert_recursive(node.left, buku)
+        elif buku.isbn > node.buku.isbn:
+            if node.right is None:
+                node.right = BSTNode(buku)
+            else:
+                return self.insert_recursive(node.right, buku)
+
+    def search_node(self, isbn):
+        node = self.root
+        while node:
+            if isbn == node.buku.isbn:
+                return node
+            elif isbn < node.buku.isbn:
+                node = node.left
+            else:
+                node = node.right
+        return None
 
     def search(self, isbn):
-        """Big-O: O(log n) rata-rata."""
-        pass # TODO: implementasikan
+        node = self.search_node(isbn)
+        return node.buku if node else None
 
     def update_status(self, isbn, status):
-        """Big-O: O(log n) rata-rata."""
-        pass # TODO: cari node, update status
+        node = self.search_node(isbn)
+        if node is None:
+            return False
+        node.buku.status = status
+        return True
 
     def inorder(self):
-        """Big-O: O(n). Kembalikan list Buku terurut ISBN."""
-        pass # TODO: implementasikan
+        result = []
+        self.inorder_recursive(self.root, result)
+        return result
+
+    def inorder_recursive(self, node, result):
+        if node is None:
+            return
+        self.inorder_recursive(node.left, result)
+        result.append(node.buku)
+        self.inorder_recursive(node.right, result)
+
+    def delete(self, key):
+        """Hapus buku dari BST berdasarkan ISBN"""
+        self.root = self._delete_recursive(self.root, key)
+
+    def _delete_recursive(self, node, key):
+        if node is None:
+            return None
+        if key < node.buku.isbn:
+            node.left = self._delete_recursive(node.left, key)
+        elif key > node.buku.isbn:
+            node.right = self._delete_recursive(node.right, key)
+        else:
+            if node.left is None and node.right is None:
+                return None
+            elif node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            else:
+                successor = self._find_min(node.right)
+                node.buku = successor.buku
+                node.right = self._delete_recursive(node.right, successor.buku.isbn)
+        return node
+
+    def _find_min(self, node):
+        while node.left:
+            node = node.left
+        return node
+
 
 class GraphRekBuku:
     """Graf tak-berarah berbobot: edge (A,B,w) = ko-pinjam frekuensi w."""
